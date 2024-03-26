807520f2d118b10c5ec39a18383af3cb3e67dc8c
wanderlust-li <zhekalichm4n@icloud.com>
2024-03-26 17:48:50 +0200

    Update web project

--- Notes.Web/Models/DTO/NoteCreateDTO.cs
+++ Notes.Web/Models/DTO/NoteCreateDTO.cs
@@ -1,7 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace Notes.Web.Models.DTO;
 
 public class NoteCreateDTO
 {
+    [JsonPropertyName("title")] // Or [JsonProperty("title")] if using Newtonsoft.Json
     public string Title { get; set; }
+
+    [JsonPropertyName("content")] // Or [JsonProperty("content")] if using Newtonsoft.Json
     public string Content { get; set; }
 }
--- Notes.Web/Services/IServices/INoteService.cs
+++ Notes.Web/Services/IServices/INoteService.cs
@@ -1,3 +1,5 @@
+using Notes.Web.Models.DTO;
+
 namespace Notes.Web.Services.IServices;
 
 public interface INoteService
@@ -5,4 +7,8 @@ public interface INoteService
     Task<T> GetAllAsync<T>();
     
     Task<T> GetAsync<T>(int id);
+    
+    Task<T> CreateAsync<T>(NoteCreateDTO dto);
+    
+    
 }
